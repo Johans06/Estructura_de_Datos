06c43b5ba42b9db0dfe12dab80f0eb7d830ef206
Johans06 <jstevenssancheza7@gmail.com>
2024-09-18 15:40:46 -0500

    Nombre_Variables

---
+++ Nombre_Variables.py
@@ -0,0 +1,9 @@
+variable = "Johan" 
+variable_2 = "Stevens" 
+VAR = "Sanchez"
+print(variable)
+print(variable_2)
+print(VAR)
+# un nombre debe comenzar con una letra o caracter 
+# una variable puede llevar numero y rayas bajas
+# una variable no puede llevar comenzar por un numero 
