bbb8df2fb2c6fcd7ca588e33bdd20dd401205121
Johans06 <jstevenssancheza7@gmail.com>
2024-09-18 15:30:08 -0500

    2_Variables

---
+++ Variables.py
@@ -0,0 +1,26 @@
+a = "Johan"
+b = 6
+print(a)
+print(b)
+
+c = int(6)      # c sera 6
+d = float(6)    # d sera 6.0
+e = str(6)      # e sera '6'
+print(c)
+print(d)
+print(e)
+
+f = 5
+g = "John"
+print(type(f))
+print(type(g))
+
+h = "Johan"
+# es lo mismo que 
+i = 'Johan'
+
+j = 4
+J = "Johan"
+# j y J son diferentes variables
+print(j)
+print(J)
