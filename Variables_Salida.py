153b57033318bd40c754944767d86797c600772a
Johans06 <jstevenssancheza7@gmail.com>
2024-09-19 14:31:35 -0500

    5_Variables_Salida

---
+++ Variables_Salida.py
@@ -0,0 +1,17 @@
+x = "Johan Stevens Sanchez"
+print(x)  # Esta funcion se utiliza para imprimir variables
+
+a = "Johan  "
+b = "Stevens "
+c = "Sanchez"
+print(a + b + c) # se puede usar (+) para darle salida a varias variables
+print(a , b , c) # tambien se puede usar (,) para darle salidas a varias variables
+
+d = 10
+e = 6
+print(a + b) #Para los números el (+) funciona como un operador matemático
+             #no se puede usar el (+) caundo queremos imprimir una cadena y un numero            
+
+f = 5
+g = "Johan"   
+print(f , g) #para este caso se utiliza mejor (,)
