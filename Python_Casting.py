578eb974e587b8c5d3ebc932c146bb8882584a47
Johans06 <jstevenssancheza7@gmail.com>
2024-09-19 15:49:25 -0500

    9_Python_Casting

---
+++ Python_Casting.py
@@ -0,0 +1,21 @@
+#construye un número entero a partir de un entero, un  flotante, 
+# o una cadena (siempre que la cadena represente un número entero)
+a = int(1)   # x sera 1
+b = int(2.8) # y sera 2
+c = int("3") # z sera 3
+print(a,b,c)
+
+#construye un número de flotador a partir de un literal de entero, un literal de flotador 
+# o un literal de cadena (siempre que la cadena represente un flotador o un entero)
+d = float(1)     # x sera 1.0
+e = float(2.8)   # y sera 2.8
+f = float("3")   # z sera 3.0
+g = float("4.2") # w sera 4.2
+print(d,e,f,g)
+
+#construye una cadena a partir de una amplia variedad de tipos de datos, 
+# incluidas cadenas, literales enteros y literales flotantes
+h = str("s1") # x sera 's1'
+i = str(2)    # y sera '2'
+j = str(3.0)  # z sera '3.0'
+print(h,i,j)
